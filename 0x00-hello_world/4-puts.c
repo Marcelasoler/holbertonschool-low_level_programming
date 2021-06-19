9a9bedd3e7158bf9bae220b1db09f967ed1b7745
Marcelasoler <marcelasoler1@hotmail.com>
2021-06-19 07:50:17 -0700

    Modifyin 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,6 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 
  */
-
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
